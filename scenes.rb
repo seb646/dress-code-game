d01ecf8ec85f51195fa49b2af03e8ae4fa1dba9b
Sebastian Rodriguez <srodriguez@browning.edu>
2017-12-04 00:54:56 -0500

    Updates

--- scenes.rb
+++ scenes.rb
@@ -10,10 +10,10 @@ end
 class Detention < Scene
     
   @@quips = [
-    "You've been sent to Mr. Reynolds office with a detention. Don't worry, they
+    "You've been sent to Mr. Beynolds office with a detention. Don't worry, they
         don't actually do anything to you - it's just a formality.",
      "Just wait until you're parents find out you got a detention. Tsk tsk.",
-     "Uh oh, guess you gotta spend 5 mins cleaning the chem lab now! ¯\_(ツ)_/¯"
+     "Uh oh, guess you gotta spend 5 mins cleaning the chemistry lab now! ¯\_(ツ)_/¯"
   ]
 
   def enter()
@@ -64,16 +64,23 @@ end
 class Advisory < Scene
 
   def enter()
-    puts "SOMETHING SOMETHING SOMETHING Ms. Ryan"
+    puts "'Hello, my precious angels,' says the unmistakable voice of Ms. Bryan."
+    puts "As she walks in the room, her eyes lock on you. You nod at her and force"
+    puts "a smile.\n"
+    puts "'Good morning, Ms. Bryan,' you tell her.\n"
+    puts "'Myles...do you want to tell me why you're out of dress code?' she asks.\n\n"
+    puts "What is your response?\n"
+    puts "1. Not really, no.\n"
+    puts "2. I woke up late and I couldn't find my blazer.\n"
     print "> "
 
     action = $stdin.gets.chomp
 
-    if action == "throw the bomb"
+    if action == "1"
       puts ""
       return 'detention'
 
-    elsif action == ""
+    elsif action == "2"
       return 'precalc'
     else
       puts "DOES NOT COMPUTE!"
@@ -85,17 +92,27 @@ end
 class Precalc < Scene
 
   def enter()
-    puts "SOMETHING SOMETHING SOMETHING Ms. Resika"
-    code = "#{rand(1..9)}#{rand(1..9)}#{rand(1..9)}"
-    puts "The code is " + code
-    print "[keypad]> "
+    puts "Your next class is Pre-Calculus with Ms. Razka. As you step into"
+    puts "room 202 you are welcomed with a certain musk unlike any other room"
+    puts "in the school. You wonder if there's a decomposing body hiding in the"
+    puts "closet. As you take your seat in the back of the room, Ms. Razka walks"
+    puts "through the door and makes her way to the desk. As she sits down, you"
+    puts "both lock eyes. Her eyes widen and a smile begins to stretch across her"
+    puts "face.\n\n"
+    puts "'Myles,' she tells you. 'Are we really going to have to do this again?'\n\n"
+    puts "'I don't know, are we?' you reply.\n\n"
+    puts "'I'll tell you what,' she says. 'If you can tell me what the answer to the"
+    puts "square root of 1888, rounded to the nearest hundredth, then I'll let you off"
+    puts "this one time.'"
+    code = "43.45"
+    print "> "
     guess = $stdin.gets.chomp
     guesses = 0
 
     while guess != code && guesses < 10
       puts "BZZZZEDDD!"
       guesses += 1
-      print "[keypad]> "
+      print "> "
       guess = $stdin.gets.chomp
     end
 
@@ -112,26 +129,25 @@ end
 class Hallway < Scene
 
   def enter()
-    puts "SOMETHING SOMETHING SOMETHING Mr. Klein"
+    puts "You finally have a free period where you can just relax and not have"
+    puts "to worry about teachers. You walk down towards the locker room, since"
+    puts "the school doesn't have a senior lounge. Right as you turn the door handle"
+    puts "you hear someone clear their throat behind you. You turn around and see"
+    puts "Mr. Clean standing in front of you smiling and nodding his head.\n\n"
+    puts "'Dude,' he tells you. 'Why aren't you getting the message? Wear your blazer man!'"
+    puts "'I'll tell you what. I'm thinking of a number between one and five. If you guess it"
+    puts "right, I'll let you off. If you don't, I'm giving you a detention. So?'"
 
     good_pod = rand(1..5)
-    puts good_pod
     print "> "
     guess = $stdin.gets.chomp.to_i
 
     if guess != good_pod
-      puts "You jump into pod %s and hit the eject button." % guess
-      puts "The pod escapes out into the void of space, then"
-      puts "implodes as the hull ruptures, crushing your body"
-      puts "into jam jelly."
       return 'detention'
     else
-      puts "You jump into pod %s and hit the eject button." % guess
-      puts "The pod easily slides out into space heading to"
-      puts "the planet below.  As it flies to the planet, you look"
-      puts "back and see your ship implode then explode like a"
-      puts "bright star, taking out the Gothon ship at the same"
-      puts "time.  You won!"
+      puts "'I'll let you off this time, but don't let me catch you without a blazer again.'\n\n"
+      puts "You exhale and finally open the door to the locker room. You toss your bag to the side"
+      puts "and sit down on the bench to relax."
       return 'finished'
     end
   end
@@ -139,6 +155,6 @@ end
 
 class Finished < Scene
   def enter()
-    puts "You won! Good job."
+    puts "Congratulations. You've made it through three periods out of dress code."
   end
 end
